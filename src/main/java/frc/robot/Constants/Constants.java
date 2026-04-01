885fa8d95a9f5fd272669f4c94f5924894692751
Maykel Silalahi <maykelsilalahi6@gmail.com>
2026-04-01 16:20:10 -0400

    Updated constants file #11

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -16,10 +16,15 @@ public class Constants {
 
         //  Intake Ramp Time Value
         public static final double kIntakeRampTime = 1;
+
     }
 
     //  2 Motors for the Spindexter Subsystem
     public static class SpindexterConstants {
+
+        //Spindexter Physical Measurements
+        public static final double kOmniwheelRadiusInInches = 3.0;
+        public static final double kTunnelFlywheelRadiusInInches = 1.0;
 
         // Spindexter Motor IDs
         public static final int kIndexerMotorID = 16;
@@ -27,7 +32,9 @@ public class Constants {
 
         //  Spindexter Current Limits
         public static final int kIndexerStatorCurrentLimit = 40;
+        public static final int kIndexerSupplyCurrentLimit = 40;
         public static final int kTunnelStatorCurrentLimit = 40;
+        public static final int kTunnelSupplyCurrentLimit = 40;
 
         //  Spindexter Speed Multipliers
         public static final double kIndexerSpeedMultiplier = 1;
@@ -36,6 +43,21 @@ public class Constants {
         //  Spindexter Ramp Time Value
         public static final double kIndexerRampTime = 0.5;
         public static final double kTunnelRampTime = 0.5;
+
+        //  ------Spindexter PID Values------
+
+        //  Indexer PID Values
+        public static final double kIndexerV = 0.12;
+        public static final double kIndexerP = 0.11;
+        public static final double kIndexerI = 0.0;
+        public static final double kIndexerD = 0.0;
+
+        //  Tunnel PID Values
+        public static final double kTunnelV = 0.12;
+        public static final double kTunnelP = 0.11;
+        public static final double kTunnelI = 0.0;
+        public static final double kTunnelD = 0.0;
+
     }
 
     //  2 Motors for Turret Subsystem
@@ -52,5 +74,14 @@ public class Constants {
         //  Turret Speed Multipliers
         public static final double kTurretShooterSpeedMultiplier = 1;
         public static final double kTurretBaseSpeedMultiplier = 1;
+
+        //  Turret Base Angle Limitations
+        public static final int kTurretBaseForwardSoftLimitThreshold = 10;
+        public static final int kTurretBaseReverseSoftLimitThreshold = -10;
+
+        //  Turret Base PID values
+        public static final double kP = 0.0015;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
     }
 }
